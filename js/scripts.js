fd64e46f2aed24b088c3162364c51f7a78f9efe5
Josh Huffman and Nhat Hoang <nhat@>
2016-08-23 09:23:12 -0700

    Use .not() function to remove clone input bars from the webpage.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 // bussiness Logic
+function resetFields (){
+  $(".new-address").not(".original").remove();
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-type").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+};
+
 function Contact(first,last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
   console.log(first +" "+last);
+};
+function Address(building, street, city, state) {
+  this.building = building;
+  this.street = street;
+  this.city = city;
+  this.state = state;
 };
  Contact.prototype.fullName = function() {
    return this.firstName + " " + this.lastName;
@@ -10,16 +27,20 @@ function Contact(first,last) {
 // user interface Logic
 $(document).ready(function() {
   $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-                                 '<div class="form-group">' +
+    $("#new-addresses").append('<div class="new-address clone">' +
+                                  '<div class="form-group clone">' +
+                                    '<label for="new-type">Type</label>'+
+                                    '<input type="text" class="form-control new-type">'+
+                                  '</div>'+
+                                 '<div class="form-group clone">' +
                                    '<label for="new-street">Street</label>' +
                                    '<input type="text" class="form-control new-street">' +
                                  '</div>' +
-                                 '<div class="form-group">' +
+                                 '<div class="form-group clone">' +
                                    '<label for="new-city">City</label>' +
                                    '<input type="text" class="form-control new-city">' +
                                  '</div>' +
-                                 '<div class="form-group">' +
+                                 '<div class="form-group clone">' +
                                    '<label for="new-state">State</label>' +
                                    '<input type="text" class="form-control new-state">' +
                                  '</div>' +
@@ -34,37 +55,29 @@ $(document).ready(function() {
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $(".new-address").each(function() {
+      var inputtedType = $(this).find("input.new-type").val();
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new address(inputtedStreet, inputtedCity, inputtedState)
-      newContact.addresses.push(newAddress)
+      var newAddress = new Address(inputtedType, inputtedStreet, inputtedCity, inputtedState)
+      newContact.addresses.push(newAddress);
     });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
+    $(".contact").last().hover(function(event){
+      event.preventDefault();
+      alert("hi");
+    });
     $(".contact").last().click(function() {
-      $("#show-contact").show();
+      $("#show-contact").fadeIn().css("background-color", "red");
       $("#show-contact h2").text(newContact.fullName());
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
       $("ul#addresses").text("");
-      newContact.addresses.forEach(function(Address) {
-        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.building + ": " + address.street + ", " + address.city + " " + address.state + "</li>");
       });
     });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
-
-    // $(".contact").last().click(function() {
-    //   $("#show-contact").show();
-    //   $("#show-contact h2").text(newContact.firstName);
-    //   $(".first-name").text(newContact.firstName);
-    //   $(".last-name").text(newContact.lastName);
-    // });
+    resetFields();
   });
 });
